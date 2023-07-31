523e64eb59bb0228f02e5b4312e7e87582c03480
realesssst <139008025+realesssst@users.noreply.github.com>
2023-07-31 12:40:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+
+def get_birthdays_per_week(users):
+    current_date = datetime.now()
+    week_later = current_date + timedelta(days=7)
+
+    this_week_birthdays = {i: [] for i in range(7)}
+
+    for user in users:
+        user["birthday"] = datetime(user["birthday"].year, user["birthday"].month, user["birthday"].day)
+        birthday = user["birthday"]
+
+        if current_date <= birthday <= week_later:
+            week_day = birthday.weekday()
+            week_day = 0 if week_day >= 5 else week_day
+
+            this_week_birthdays[week_day].append(user['name'])
+
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+
+    for index, names in this_week_birthdays.items():
+        if names:
+            day = days[index]
+            print(f"{day}: {', '.join(names)}")
+
+users = [
+    {"name": "John", "birthday": datetime(1995, 7, 31)},
+    {"name": "Emily", "birthday": datetime(1980, 7, 31)},
+    {"name": "Chris", "birthday": datetime(1998, 8, 1)},
+    {"name": "Anna", "birthday": datetime(1972, 8, 3)},
+    {"name": "David", "birthday": datetime(2001, 8, 3)},
+    {"name": "Sophia", "birthday": datetime(1999, 8, 5)},
+    {"name": "Michael", "birthday": datetime(1985, 8, 7)},
+]
+
+get_birthdays_per_week(users)
